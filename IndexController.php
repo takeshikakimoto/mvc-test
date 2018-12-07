b47462d9c726607b4d7e50ca53f227ed3d641653
takeshikakimoto <takeshi_kakimoto@rudel.jp>
2018-12-07 17:01:48 +0900

    謎が解けました

--- IndexController.php
+++ IndexController.php
@@ -5,9 +5,11 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
+
         try {$pdo = new PDO("mysql:host=localhost;dbname=board_test;charset=utf8", "root", "");
             $sql = "SELECT * FROM board";
             $stmt = $pdo->query($sql);
+            $this->view->setVar('hoge', $stmt);
         } catch (PDOException $e) {
 
             echo 'データベースにアクセスできません！';
